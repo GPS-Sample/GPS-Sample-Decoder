5f2b792f5e13128df0162687daf9dbf8ef8eae03
Russell <5808223+RussMitch@users.noreply.github.com>
2025-08-24 09:54:24 -0500

    Added GPLv3 license

--- GPSSampleDecoder/App.xaml.cs
+++ GPSSampleDecoder/App.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2022-2025 Georgia Tech Research Institute
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * See the LICENSE file for the full license text.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
