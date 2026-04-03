91c1d28d0b18b2de6705b2616a0ade0f49e497e2
RussMitch <5808223+RussMitch@users.noreply.github.com>
2026-04-03 07:55:46 -0500

    Added cap to save images without a config

--- GPSSampleDecoder/Utils/SaveUtils.cs
+++ GPSSampleDecoder/Utils/SaveUtils.cs
@@ -37,26 +37,75 @@ namespace GPSSampleDecoder.Utils
 
         public SaveStateError SaveOutput(string path, string json, Configuration combinedConfiguration, List<Configuration> configurations, ImageList imageList, SaveState mode)
         {
-            // based on mode, save either xls, csv or throw error
-            var configName = "Config";
-
             if (combinedConfiguration != null && !String.IsNullOrEmpty(combinedConfiguration.name))
             {
-                configName = combinedConfiguration.name;
-            }
+                var configName = combinedConfiguration.name;
 
-            if (mode == SaveState.Xls || mode == SaveState.CsvAndXls)
-            {
+                // Write Excel File
                 excelWriter.SaveExcel(combinedConfiguration, path);
 
+                // Write JSON File
+                using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(path, configName + ".json")))
+                {
+                    outputFile.WriteLine(json);
+                }
+
+                // Write GPX Files
+                var bodyText = "";
+                foreach (var enumArea in combinedConfiguration.enumAreas)
+                {
+                    // group breadcrumbs by groupId
+
+                    if (enumArea.breadcrumbs.Count() > 1)
+                    {
+                        var groupId = enumArea.breadcrumbs.First().groupId;
+
+                        List<Breadcrumb> breadcrumbs = new List<Breadcrumb>();
+                        List<List<Breadcrumb>> breadcrumbGroups = new List<List<Breadcrumb>>();
+
+                        foreach (var breadcrumb in enumArea.breadcrumbs)
+                        {
+                            if (breadcrumb.groupId == groupId)
+                            {
+                                breadcrumbs.Add(breadcrumb);
+
+                                if (breadcrumb == enumArea.breadcrumbs.Last())
+                                {
+                                    breadcrumbGroups.Add(breadcrumbs);
+                                }
+                            }
+                            else
+                            {
+                                groupId = breadcrumb.groupId;
+                                breadcrumbGroups.Add(breadcrumbs);
+                                breadcrumbs = new List<Breadcrumb>() { breadcrumb };
+                            }
+                        }
+
+                        foreach (var breadcrumbGroup in breadcrumbGroups)
+                        {
+                            bodyText += createTrk(enumArea.name, breadcrumbGroup, combinedConfiguration.timeZone);
+                        }
+                    }
+                }
+
+                if (bodyText.Length > 0)
+                {
+                    var fileText = "";
+
+                    fileText += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "\n";
+                    fileText += "<gpx version=\"1.1\" creator=\"GPSSample\"" + "\n";
+                    fileText += "xmlns=\"http://www.topografix.com/GPX/1/1\">" + "\n";
+                    fileText += bodyText;
+                    fileText += "</gpx>" + "\n";
+
+                    string outpath = System.IO.Path.Combine(path, configName + ".gpx");
+                    byte[] bytes = Encoding.UTF8.GetBytes(fileText);
+                    File.WriteAllBytes(outpath, bytes);
+                }
             }
 
-            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(path, configName + ".json")))
-            {
-                //foreach (string line in lines)
-                outputFile.WriteLine(json);
-            }
-
+            // Write Image Files
             if (imageList != null)
             {
                 foreach (var image in imageList.images)
@@ -66,62 +115,6 @@ namespace GPSSampleDecoder.Utils
                     File.WriteAllBytes(outpath, bytes);
                 }
             }
-
-            var bodyText = "";
-
-
-            foreach (var enumArea in combinedConfiguration.enumAreas)
-            {
-                // group breadcrumbs by groupId
-
-                if (enumArea.breadcrumbs.Count() > 1)
-                {
-                    var groupId = enumArea.breadcrumbs.First().groupId;
-
-                    List<Breadcrumb> breadcrumbs = new List<Breadcrumb>();
-                    List<List<Breadcrumb>> breadcrumbGroups = new List<List<Breadcrumb>>();
-
-                    foreach (var breadcrumb in enumArea.breadcrumbs)
-                    {
-                        if (breadcrumb.groupId == groupId)
-                        {
-                            breadcrumbs.Add(breadcrumb);
-
-                            if (breadcrumb == enumArea.breadcrumbs.Last())
-                            {
-                                breadcrumbGroups.Add(breadcrumbs);
-                            }
-                        }
-                        else
-                        {
-                            groupId = breadcrumb.groupId;
-                            breadcrumbGroups.Add(breadcrumbs);
-                            breadcrumbs = new List<Breadcrumb>() { breadcrumb };
-                        }
-                    }
-
-                    foreach (var breadcrumbGroup in breadcrumbGroups)
-                    {
-                        bodyText += createTrk(enumArea.name, breadcrumbGroup, combinedConfiguration.timeZone);
-                    }
-                }
-            }
-
-            if (bodyText.Length > 0)
-            {
-                var fileText = "";
-
-                fileText += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + "\n";
-                fileText += "<gpx version=\"1.1\" creator=\"GPSSample\"" + "\n";
-                fileText += "xmlns=\"http://www.topografix.com/GPX/1/1\">" + "\n";
-                fileText += bodyText;
-                fileText += "</gpx>" + "\n";
-
-                string outpath = System.IO.Path.Combine(path, configName + ".gpx");
-                byte[] bytes = Encoding.UTF8.GetBytes(fileText);
-                File.WriteAllBytes(outpath, bytes);
-            }
-
 
             return SaveStateError.Success;
 
